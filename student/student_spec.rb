36d7892ab7e91b680ae2466a25737a1b2178e5a1
Smithwebdd <smithwebdd@gmail.com>
2019-06-26 21:18:51 -0400

    refactored spec test

--- student/student_spec.rb
+++ student/student_spec.rb
@@ -1,19 +1,26 @@
 require_relative 'student'
 
 RSpec.describe Student do
-  before do
-    @student = Student.new('John', 'Doe')
-  end
+  describe 'instance methods' do
+
+    subject { Student.new('John', 'Doe') } 
+    it { respond_to(:first_name) } 
+    it { respond_to(:last_name) } 
+    it { respond_to(:studentFullname) } 
+    
+    # before do
+    #   @student = Student.new('John', 'Doe')
+    # end
+    # it 'Should respond to #first_name' do
+    #   expect(@student).to respond_to(:first_name)
+    # end
   
-  it 'Should respond to #first_name' do
-    expect(@student).to respond_to(:first_name)
-  end
-
-  it 'Should respond to #last_name' do
-    expect(@student).to respond_to(:last_name)
-  end
-
-  it 'Should respond to #studentFullname' do
-    expect(@student).to respond_to(:studentFullname)
+    # it 'Should respond to #last_name' do
+    #   expect(@student).to respond_to(:last_name)
+    # end
+  
+    # it 'Should respond to #studentFullname' do
+    #   expect(@student).to respond_to(:studentFullname)
+    # end
   end
 end
